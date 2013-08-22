35b3485e4726bfb8d60a2dbb581a35abadfe16ba
Ryan Hoegg <ryan.hoegg@gmail.com>
2013-08-22 17:03:17 -0500

    test case demonstrating thread leak

--- src/test/java/org/mule/module/mongo/automation/testcases/PoolingTestCases.java
+++ src/test/java/org/mule/module/mongo/automation/testcases/PoolingTestCases.java
@@ -9,6 +9,7 @@
 package org.mule.module.mongo.automation.testcases;
 
 import com.mongodb.BasicDBObject;
+import junit.framework.AssertionFailedError;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,10 @@ import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
 import org.mule.api.processor.MessageProcessor;
 import org.mule.api.transport.Connector;
+import org.mule.util.StringUtils;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,6 +29,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class PoolingTestCases extends MongoTestParent {
+    long[] currentThreads;
 
     @SuppressWarnings("unchecked")
     @Before
@@ -52,7 +57,7 @@ public class PoolingTestCases extends MongoTestParent {
 
     @Category({ RegressionTests.class })
     @Test
-    public void testPoolSizeDoesNotExceedConfiguration() throws Exception {
+    public void poolSizeDoesNotExceedConfiguration() throws Exception {
 
         int numObjects = (Integer) testObjects.get("numObjects");
 
@@ -74,4 +79,48 @@ public class PoolingTestCases extends MongoTestParent {
 
         int newConnections = lookupFlowConstruct("count-open-connections").process(getTestEvent("")).getMessage().getPayload(Integer.class) - startingConnections;
         assertTrue("Too many new connections (" + newConnections + ", ", newConnections <= 2);
-    }}
+    }
+
+    @Category({RegressionTests.class})
+    @Test
+    public void mongoShouldBeResponsibleWithThreads() {
+        int numObjects = (Integer) testObjects.get("numObjects");
+
+        insertObjects(getEmptyDBObjects(numObjects));
+
+        currentThreads = ManagementFactory.getThreadMXBean().getAllThreadIds();
+
+        MessageProcessor countFlow = lookupFlowConstruct("count-objects");
+        testObjects.put("queryRef", new BasicDBObject());
+
+        for (int i = 0; i < 100; i++) {
+            try {
+                countFlow.process(getTestEvent(testObjects));
+            } catch (Exception e) {
+                e.printStackTrace();
+                fail();
+            }
+
+        }
+
+        ThreadInfo[] newThreads = ManagementFactory.getThreadMXBean().getThreadInfo(getNewThreads());
+
+        assertTrue("Too many new threads (" + newThreads.length + ")", newThreads.length <= 2);
+    }
+
+    private long[] getNewThreads() {
+        List<Long> threads = new LinkedList<Long>();
+        for (long l : ManagementFactory.getThreadMXBean().getAllThreadIds()) {
+            threads.add(new Long(l));
+        }
+        for (long l : currentThreads) {
+            threads.remove(new Long(l));
+        }
+        long[] result = new long[threads.size()];
+        for (int i = 0; i < threads.size(); i++) {
+            result[i] = threads.get(i);
+        }
+        return result;
+    }
+
+}
